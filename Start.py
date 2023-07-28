33989749ebdbaf368930905d908c33b541b9e9dd
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-07-27 20:55:02 -0500

    commit on master

--- Start.py
+++ Start.py
@@ -15,20 +15,31 @@ else:
 # Search for RobloxPlayerBeta.exe in the Versions directory and its subdirectories
 Roblox_Player_Location = None
 for root, dirs, files in os.walk(Roblox_Versions_Location):
-    if 'RobloxPlayerBeta.exe' in files:
-        Roblox_Player_Location = os.path.join(root, 'RobloxPlayerBeta.exe')
+    if "RobloxPlayerBeta.exe" in files:
+        Roblox_Player_Location = os.path.join(root, "RobloxPlayerBeta.exe")
         break
 
 # Check if Roblox Player exists before trying to run it
 if Roblox_Player_Location and os.path.exists(Roblox_Player_Location):
     # Start Roblox Player and do not wait for it to finish
+    print(Roblox_Player_Location)
     process = subprocess.Popen(Roblox_Player_Location)
+
 else:
     print(f"{Roblox_Player_Location} does not exist.")
 
 # Wait for the Roblox Player process to finish
-while process and process.poll() is None:
-    pass
+while process:
+    try:
+        if process.poll() is not None:
+            break
+    except OSError as e:
+        if e.winerror == 6:  # The handle is invalid
+            print("winerror detected")
+            break
+        else:
+            raise
+
 
 def Stop():
     if os.path.exists(SteamVr1_Location):
@@ -36,5 +47,6 @@ def Stop():
         print("Complete")
     else:
         print(f"{SteamVr1_Location} does not exist.")
-    
+
+
 Stop()
